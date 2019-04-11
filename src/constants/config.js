df653c13320e504de2f4385ca7fe420dc503c7d8
Tyler Burnam <tyler@burnamtech.com>
2019-04-11 17:39:15 -0400

    Removed not needed dependencies

--- src/constants/config.js
+++ src/constants/config.js
@@ -11,5 +11,5 @@ export default {
   gaTrackingId: (devMode) ? 'UA-84284256-2' : 'UA-84284256-1',
 
   // TODO: Need to manually update this each time you run ngrok
-  ROOT_URL: 'http://581618f4.ngrok.io',
+  ROOT_URL: 'http://ea6f9d84.ngrok.io',
 };
