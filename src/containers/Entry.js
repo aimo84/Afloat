fb03f0f7914a21e2a5352953a4e82cf61acb40d1
Joe Connolly <jcjconnolly4@gmail.com>
2019-04-19 14:30:38 -0400

    reverted changes to carousel

--- src/containers/Entry.js
+++ src/containers/Entry.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Container, Content, Text, Button,
+  Container, Content,
 } from 'native-base';
 // import PlaidAuthenticator from 'react-native-plaid-link';
 import { connect } from 'react-redux';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
-import { Actions } from 'react-native-router-flux';
 
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
 import { getRecipes, getMeals, setError } from '../actions/recipes';
 import Cards from '../native/components/Cards';
 import all from './styles.js';
 
-// const styles = StyleSheet.create({
-//   mainContainer: {
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   dayText: {
-//     fontWeight: 'bold',
-//     fontSize: 18,
-//   },
-// });
 
 class Entry extends Component {
   static propTypes = {
@@ -45,9 +34,7 @@ class Entry extends Component {
     };
   }
 
-  goToBank = () => {
-    Actions.linkBank();
-  };
+  // componentDidMount = () => this.fetchRecipes();
 
   checkState = () => {
     console.log(this.props);
@@ -77,18 +64,45 @@ class Entry extends Component {
   render = () => {
     // const { uid } = this.props.member;
     console.log(this.props);
-
+    // const { width } = Dimensions.get('window');
+    // const contentOffset = (width - CustomComponent.WIDTH) / 2;
+    // const { currentIndex } = this.state;
+    const { slider1ActiveSlide } = this.state;
+    const ENTRIES1 = [{ title: 'Quick' }, { title: 'Affordable' }, { title: 'Get Started' }];
+    const { width: viewportWidth } = Dimensions.get('window');
 
     return (
 
       <Container style={{ marginTop: 22 }}>
-        <Content>
-          <Text>Please Link your bank account:</Text>
-          <Button onPress={() => this.goToBank()}>
-            <Text>
-            Link Bank Account
-            </Text>
-          </Button>
+        <Content style={{ flex: 1 }}>
+          <Carousel
+            layout="stack"
+            // eslint-disable-next-line
+              ref={c => this._slider1Ref = c}
+            data={ENTRIES1}
+            renderItem={this._renderItemWithParallax}
+            sliderWidth={viewportWidth}
+            itemWidth={this.wp(120)}
+            firstItem={0}
+            inactiveSlideScale={0.94}
+            inactiveSlideOpacity={0.7}
+                      // inactiveSlideShift={20}
+                      // containerCustomStyle={styles.slider}
+                      // contentContainerCustomStyle={styles.sliderContentContainer}
+            onSnapToItem={index => this.setState({ slider1ActiveSlide: index })}
+          />
+          <Pagination
+            dotsLength={ENTRIES1.length}
+            activeDotIndex={slider1ActiveSlide}
+              // containerStyle={styles.paginationContainer}
+            dotColor="rgba(255, 255, 255, 0.92)"
+              // dotStyle={styles.paginationDot}
+            inactiveDotColor="#000000"
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+            carouselRef={this._slider1Ref}
+            tappableDots={!!this._slider1Ref}
+          />
         </Content>
       </Container>
     );
