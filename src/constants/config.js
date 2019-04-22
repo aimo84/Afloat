11c268880f456fc7165ea13f1a50f049297d4a07
Joe Connolly <jcjconnolly4@gmail.com>
2019-04-22 16:27:10 -0400

    changed ngrok again

--- src/constants/config.js
+++ src/constants/config.js
@@ -11,5 +11,5 @@ export default {
   gaTrackingId: (devMode) ? 'UA-84284256-2' : 'UA-84284256-1',
 
   // TODO: Need to manually update this each time you run ngrok
-  ROOT_URL: 'http://9ef1e6d8.ngrok.io',
+  ROOT_URL: 'http://9ef6d8.ngrok.io',
 };
