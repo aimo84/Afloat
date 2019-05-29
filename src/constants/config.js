52de54222b39bb52e721f278828a48dfddc86a56
Azhar Hussain <azhar.m.hussain.19@dartmouth.edu>
2019-05-29 14:22:51 -0400

    cleaning up

--- src/constants/config.js
+++ src/constants/config.js
@@ -11,5 +11,5 @@ export default {
   gaTrackingId: (devMode) ? 'UA-84284256-2' : 'UA-84284256-1',
 
   // TODO: Need to manually update this each time you run ngrok
-  ROOT_URL: 'https://3975ceb2.ngrok.io',
+  ROOT_URL: 'https://350a311d.ngrok.io',
 };
